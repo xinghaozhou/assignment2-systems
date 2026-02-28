2d31844fbd66fd147848608cf95cf9069521e4e3
xinghaozhou <145933090+xinghaozhou@users.noreply.github.com>
2026-02-28 18:00:32 -0500

    Update and rename benchmark_memory to benchmark_memory.sh

---
+++ script/benchmark_memory.sh
@@ -0,0 +1,13 @@
+uv run nsys profile -o result python -m cs336_systems.benchmark \
+    --device cuda \
+    --dtype float32 \
+    --size 2.7B \
+    --use_bf16 True \
+    --memory_record True \
+    --d_model 2560 \
+    --d_ff 10240 \
+    --num_layers 32 \
+    --num_heads 32 \
+    --warmup_steps 5 \
+    --pass_type both \
+    --test_steps 10 \
